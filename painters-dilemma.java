95bbf7d19e5eb62731b9dc1c4ba317c9d3becb92
frankelid12 <45718604+frankelid12@users.noreply.github.com>
2019-08-21 15:07:00 +0300

    Update painters-dilemma.java

--- painters-dilemma.java
+++ painters-dilemma.java
@@ -1,55 +1,66 @@
-import java.io.*;
-import java.util.*;
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
-public class Solution {
-    static int N;
-    static List<Integer> colors;
-    
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        for (int i = 0; i < t; ++i) {
-            N = in.nextInt();
-            colors = new ArrayList<>();
-            for (int j = 0; j < N; ++j) {
-                colors.add(in.nextInt());
-            }
-            testCase();
+const int num_color = 20;
+const int max_change = 100000;
+const int max_time = 501;
+
+int minChange(vector<int> &colors) {
+    int count[max_time][num_color+1][num_color+1];
+    for(int c1=0; c1<num_color+1; c1++) {
+        for(int c2=0; c2<num_color+1; c2++) {
+            count[0][c1][c2] = max_change;
         }
     }
-    
-    static void testCase() {
-        int[] colorCount = new int[21];
-        int color = 0;
-        for (int c : colors) {
-            if (c == color)
-                continue;
-            int min = Integer.MAX_VALUE;
-            for (int i = 0; i < 21; ++i) {
-                if (colorCount[i] != 0 && i != color && i != c) {
-                    if (colorCount[i] < min)
-                        min = colorCount[i];
-                    ++colorCount[i];
+    count[0][num_color][num_color] = 0;
+
+    for(int t=1; t<=colors.size(); t++) {
+        for(int c1=0; c1<num_color+1; c1++) {
+            for(int c2=0; c2<num_color+1; c2++) {
+                int min_change = max_change;
+                if(c1 == colors[t-1] || c2 == colors[t-1]) {
+                    for(int c=0; c<num_color+1; c++) {
+                        min_change = min(min_change, count[t-1][c1][c] + 1);
+                    }
+                    for(int c=0; c<num_color+1; c++) {
+                        min_change = min(min_change, count[t-1][c][c2] + 1);
+                    }
+                    count[t][c1][c2] = min(min_change, count[t-1][c1][c2]);
+                }
+                else {
+                    count[t][c1][c2] = max_change;
                 }
             }
-            if (colorCount[c] != 0) {
-                colorCount[color] = Math.min(colorCount[c], Math.max(min+1, 1));
-            } else  {
-                colorCount[color] = Math.max(min+1, 1);
-            }
-            colorCount[c] = 0;
-            color = c;
         }
-        
-        int min = Integer.MAX_VALUE;
-        for (int count : colorCount) {
-            if (count != 0 && count < min)
-                min = count;
-        }
-        
-        if (min == Integer.MAX_VALUE)
-            min = 0;
-        
-        System.out.println(min);
     }
+    
+    int min_change = max_change;
+    for(int c1=0; c1<num_color+1; c1++) {
+        min_change = min(min_change, count[colors.size()][c1][colors.back()]);
+        min_change = min(min_change, count[colors.size()][colors.back()][c1]);
+    }
+    return min_change;
+}
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int T;
+    cin >> T;
+    for(int t=0; t<T; t++) {
+        int n_color;
+        cin >> n_color;
+        vector<int> colors;
+        int color;
+        for(int c=0; c<n_color; c++) {
+            cin >> color;
+            colors.push_back(color-1);
+        }
+        cout << minChange(colors) << endl;
+    }
+    return 0;
 }
